7320120fba101aa1a23eaf1d2f7332bd4059b758
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 13:26:49 -0300

    ✨ feat(home_page): add adventure cards to home page

--- src/routes/Home/HomePage.tsx
+++ src/routes/Home/HomePage.tsx
@@ -1,9 +1,39 @@
-import { PageLayout } from "../../components/PageLayout";
+import { PageLayout, PageTitle } from "../../components/PageLayout";
+import { AdventureCard } from "../../components/AdventureCard";
+import { useEffect, useState } from "react";
+import { AdventureCardType } from "../../types/AdventureCardType";
+import { useApi } from "../../hooks/useApi";
 
 export const HomePage = () => {
+    const api = useApi();
+    const [adventures, setAdventures] = useState<AdventureCardType[]>([]);
+
+    async function getUserAdventures() {
+        const userAdventures = await api.getUserAdventures('');
+        console.log(userAdventures)
+        setAdventures(userAdventures)
+    }
+
+    useEffect(() => {
+        getUserAdventures();
+    }, [])
+
     return (
         <PageLayout>
-            <div>oi</div>
+            <>
+                <PageTitle title="Minhas Aventuras" />
+                <div className="w-full">
+                    <ul className="pt-10 grid grid-cols-3">
+                        {adventures.map((adventure) => {
+                            return (
+                                <li key={adventure.id}>
+                                    <AdventureCard adventure={adventure} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </>
         </PageLayout>
     )
 }
