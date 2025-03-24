208c80534a1f16b2eebabe5131b4f69cc28eabb6
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 09:50:02 -0300

    ✨ feat(page_layout): add main content to page layout

---
+++ src/components/PageLayout/components/MainContent.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "react";
+
+interface Props {
+    children: JSX.Element
+}
+
+const MainContent = (props: Props) => {
+    return (
+        <main className="w-full h-[calc(100%-60px)] px-8 py-4 overflow-y-auto">
+            {props.children}
+        </main>
+    )
+}
+
+export default MainContent;
