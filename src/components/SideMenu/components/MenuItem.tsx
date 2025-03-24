15d4aa9d180d96d8069efeda7c24e5406faed65e
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 08:42:00 -0300

    ✨ feat(home_page): add active marker to menu item

--- src/components/SideMenu/components/MenuItem.tsx
+++ src/components/SideMenu/components/MenuItem.tsx
@@ -6,9 +6,10 @@ interface Props {
 
 const MenuItem = (props: Props) => {
     return (
-        <button className={`w-[240px] h-[47px] rounded-[15px] flex gap-8 items-center text-secondary-content border cursor-pointer ${props.active ? 'bg-secondary/20 border-secondary' : 'border-neutral/26 hover:bg-neutral/10'}`}>
+        <button className={`select-none relative w-[240px] h-[47px] rounded-[15px] flex gap-8 items-center text-secondary-content border cursor-pointer ${props.active ? 'bg-secondary/20 border-secondary' : 'border-neutral/26 hover:bg-neutral/10'}`}>
             <div className="w-[19px] px-[23px]"><props.icon /></div>
             <span className="text-[12px]">{props.label}</span>
+            <span className={`${props.active ? 'visible' : 'invisible'} w-[6px] h-[19px] bg-secondary rounded-full absolute left-[-4px]`}></span>
         </button>
     )
 }
