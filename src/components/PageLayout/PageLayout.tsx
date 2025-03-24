58709fc35f491da5b376c0b88eb3ad6f65f75f76
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 00:46:30 -0300

    ✨ feat(home_page): added Side Menu Component in Page Layout

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,9 +1,14 @@
 import { JSX } from "react"
+import { SideMenu } from "../SideMenu/SideMenu";
 
 interface Props {
     children: JSX.Element;
 }
 
 export const PageLayout = (props: Props) => {
-
+    return (
+        <div className="w-screen h-screen">
+            <SideMenu />
+        </div>
+    )
 }
