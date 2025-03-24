15974ead9c9ddebdf4811175cf240fc8be0f1e7e
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 08:37:40 -0300

    ✨ feat(home_page): add logo to side menu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router";
 import MenuItem from "./components/MenuItem";
 import { sideMenuGlobalRoutes } from "./SideMenuConfig";
+import LogoSM from "../../assets/images/logo_sm.png";
 
 export const SideMenu = () => {
     const globalRoutes = sideMenuGlobalRoutes;
@@ -8,7 +9,11 @@ export const SideMenu = () => {
 
     return (
         <aside className="bg-base200 w-[291px] h-full flex flex-col items-center">
-            <nav className="w-[240px]">
+            <div className="flex gap-3 text-base-content/80 font-bold text-[24px] items-center h-[64px]">
+                <img src={LogoSM}></img>
+                <span className="uppercase">Scholarium</span>
+            </div>
+            <nav className="w-[240px] pt-10">
                 <div className="text-base-content/54 uppercase text-[15px] ps-3 pb-2 font-semibold">{globalRoutes.label}</div>
                 <ul className="w-full flex flex-col gap-4">
                     {globalRoutes.routes.map((route) => {
