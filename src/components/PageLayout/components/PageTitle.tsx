fb8cadca175b780aa1718cc4d68201585d8741a4
kaikdorvalo <kaikdorvalo@gmail.com>
2025-03-24 13:20:33 -0300

    ✨ feat(page_layout): adjuste page title size

--- src/components/PageLayout/components/PageTitle.tsx
+++ src/components/PageLayout/components/PageTitle.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const PageTitle = (props: Props) => {
     return (
-        <h1 className="font-bold text-[36px] py-8">{props.title}</h1>
+        <h1 className="font-bold text-[30px] py-4 text-base-content/80">{props.title}</h1>
     )
 }
 
