6313a34b1ae8e20eac7e78057133ab0ced8ace39
Renan <tononjob@gmail.com>
2025-03-28 09:05:29 -0300

    :sparkles: feat(sign_up): add page background, page header, page information

--- src/components/PagelayoutLogin/menu.tsx
+++ src/components/PagelayoutLogin/menu.tsx
@@ -1,15 +1,15 @@
 import LogoSm from "../../assets/images/logo_sm.png"
 export const Menu = () =>{
-    return (<div className="flex items-center justify-around p-[15px] text-base-content">
+    return (<div className="flex items-center justify-between py-[15px]  px-[150px] text-base-content absolute w-full">
         <div  className="flex items-center justify-around gap-[10px]">
             <img src= {LogoSm} alt="logo pequena Scholarium" />
             <p>Scholarium</p>
         </div>
-        <div>
+        <div className="flex gap-[10px]">
             <a href="" target="_blank" rel="noopener noreferrer">Por que nós?</a>
             <a href="" target="_blank" rel="noopener noreferrer">Gamificação</a>
             <a href="" target="_blank" rel="noopener noreferrer">Planos</a>
         </div>
-        <button>Cadastrar</button>
+        <button className="border border-white p-[10px] rounded-[15px] w-[150px]">Cadastrar</button>
     </div>);
 }
--- src/routes/Registro/RegistroPage.tsx
+++ src/routes/Registro/RegistroPage.tsx
@@ -1,7 +1,22 @@
-import {Menu} from "../../components/PagelayoutLogin/Menu" 
+import {Menu} from "../../components/PagelayoutLogin/menu"
+import TreeTravaller from "../../assets/images/treeTravallers.png"
+
+import wave from "../../assets/images/wave_bg.png"
 export const Registro = () => {
-    return ( <div className="bg-base200 h-screen w-screen">
+    return ( <div className="flex bg-base200 h-screen w-screen font-poppins ">
             <Menu/>
+            <div className="w-2/5  h-full"></div>
+            <div className="flex flex-col gap-[30px] w-3/5 bg-cover bg-center h-full text-white  pt-[100px] items-end justify-center pe-[150px]" style={{ backgroundImage:`url('${wave}')` }}>
+                <img src={TreeTravaller} alt="Três Viagantes" className="flex w-[400px] " />
+                <div className="flex w-[400px] text-3xl justify-center items-end gap-[10px] text-wrap">
+                <p>
+                    Comece sua <span className="text-4xl text-primary font-bold">JORNADA</span> agora!
+                </p>
+                </div>
+
+            </div>
+            {/* <img src={WaveBg} alt="Wave" className="flex absolute right-0 h-screen " /> */}
+           
     </div>);
 }
 
