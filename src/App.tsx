e4aad2212ba9a04600473ada882b670bc4d949ec
Kaik Dorvalo <kaikdorvalo@gmail.com>
2025-03-28 09:27:48 -0300

    :truck: rename(signup): rename import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router"
 import { HomePage } from "./routes/Home/HomePage"
-import { SignUpPage } from "./routes/Registro/SignUpPage"
+import { SignUpPage } from "./routes/SignUp/SignUpPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<HomePage />} />,
-      <Route path="/signup" element={<SignUpPage/>} />
+      <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   )
 }
